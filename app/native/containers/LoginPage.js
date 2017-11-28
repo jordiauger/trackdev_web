878b5bd491807a9efb3433ec51b0346cbd789050
Ignacio Martín <ignacio.martin@udg.edu>
2017-11-28 17:00:01 +0100

    Native container for login form

--- app/native/containers/LoginPage.js
+++ app/native/containers/LoginPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { View } from 'react-native'
 import PropTypes from 'prop-types';
-import LoginForm from './LoginForm';
+import LoginForm from '../../components/LoginForm';
 import { connect } from 'react-redux';
-import { userLoginRequest } from '../actions/loginActions'
+import { userLoginRequest } from '../../actions/loginActions'
 
 class LoginPage extends React.Component{
   render() {
