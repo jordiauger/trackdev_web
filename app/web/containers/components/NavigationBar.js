46449b35d812fd488e3231ad28b415f16a4a599a
Ignacio Martín <ignacio.martin@udg.edu>
2017-11-28 17:01:56 +0100

    Reformated

--- app/web/containers/components/NavigationBar.js
+++ app/web/containers/components/NavigationBar.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import {Nav, Navbar, NavItem} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
-export default ()=> {
-    return (
-      <Navbar>
-          <Navbar.Header>
-              <Navbar.Brand>
-                  <Link to={'/'}>Home</Link>
-              </Navbar.Brand>
-          </Navbar.Header>
-          <Nav pullRight>
-              <LinkContainer to="/login">
-                <NavItem eventKey={1}>Login</NavItem>
-              </LinkContainer>
-          </Nav>
-      </Navbar>
-    );
+export default () => {
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to={'/'}>Home</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav pullRight>
+        <LinkContainer to="/login">
+          <NavItem eventKey={1}>Login</NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar>
+  );
 }
