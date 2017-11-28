4d7734140643475477a4f79f72bf5a45fed53f57
Ignacio Martín <ignacio.martin@udg.edu>
2017-11-28 19:51:41 +0100

    Reformated

--- index.android.js
+++ index.android.js
@@ -1,8 +1,8 @@
 // https://github.com/facebook/react-native/issues/9599
 if (typeof global.self === "undefined") global.self = global;
 
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
 import Root from './app/native/containers/Root'
 
 class TrackDev extends Component {
